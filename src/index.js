5274b897a56ef4abb5f56bbe287fdab894110ff1
meln-galya <meln.galya@gmail.com>
2023-01-30 00:13:37 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
+module.exports = function reverse(n) {
+    let num = Math.abs(n);
+    let result = '';
+    for (let i = String(num).length - 1; i >= 0; i--) {
+        result += String(num)[i];
+    }
+    return +result;
   
 }
